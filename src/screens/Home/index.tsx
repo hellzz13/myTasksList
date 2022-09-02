4e35b2fb760a0602e8269c78d3869f3f2f171a1f
hellzz13 <hpalenca13@gmail.com>
2022-09-02 00:12:27 -0300

    base do projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {TaskList} from '../../components/TaskList';
+import {Task} from '../../types/Task';
+
+export const Home = () => {
+  const [newTask, setNewTask] = React.useState('');
+  const [tasks, setTasks] = React.useState<Task[]>([]);
+
+  const handleAddNewTask = () => {
+    const data = {
+      id: String(new Date().getTime()),
+      title: newTask ? newTask : 'Task empty',
+    };
+    setTasks([...tasks, data]);
+  };
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Lista de tarefas</Text>
+        <TextInput
+          placeholderTextColor="#555"
+          placeholder="Nova tarefa..."
+          onChangeText={setNewTask}
+          style={styles.input}
+        />
+        <TouchableOpacity
+          onPress={handleAddNewTask}
+          style={styles.button}
+          activeOpacity={0.7}>
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+        <Text style={styles.titleTasks}>Minha tarefas</Text>
+
+        <TaskList tasks={tasks} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#121214',
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 30,
+    paddingVertical: 50,
+  },
+  title: {
+    color: '#f1f1f1',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#29292e',
+    color: '#f1f1f1',
+    fontSize: 18,
+    marginTop: 30,
+    padding: 15,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#eba417',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#121214',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  titleTasks: {
+    color: '#f1f1f1',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 50,
+  },
+  buttonTask: {
+    backgroundColor: '#29292e',
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 50,
+    alignItems: 'center',
+  },
+  titleTask: {
+    color: '#f1f1f1',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
